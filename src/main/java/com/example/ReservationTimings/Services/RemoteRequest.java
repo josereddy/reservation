2a6de5d2a953673b4464f5ce2b9b5b2aa0e3f216
jose737vijay <jose737vijay@gmail.com>
2022-02-17 17:59:41 -0500

    final commit

--- src/main/java/com/example/ReservationTimings/Services/RemoteRequest.java
+++ src/main/java/com/example/ReservationTimings/Services/RemoteRequest.java
@@ -4,6 +4,7 @@ import com.example.ReservationTimings.DTO.Remote_Put_Reservation_Menus_DTO;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +13,8 @@ import org.springframework.web.client.RestTemplate;
 public class RemoteRequest {
     //
 
+    @Value("${Remote.url_remote}")
+    private String url_remote_check;
 
     private static final Logger log = LogManager.getLogger(RemoteRequest.class.getName());
 
@@ -20,9 +23,9 @@ public class RemoteRequest {
         log.info("REMOTE REQUEST: ENTERED INTO THE CHECK_REMOTE_DATA");
         HttpHeaders headers = getHeaders();
         RestTemplate restTemplate = new RestTemplate();
-        String url = "http://${MENUS_SERVER:localhost}:8082/menus/put/check_reservation_menus";
+//        String url = "http://${MENUS_SERVER:localhost}:8082/menus/put/check_reservation_menus";
         HttpEntity<Remote_Put_Reservation_Menus_DTO> requestEntity = new HttpEntity<>(reservation_data,headers);
-        ResponseEntity<Integer> response_entity=restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Integer.class);
+        ResponseEntity<Integer> response_entity=restTemplate.exchange(url_remote_check, HttpMethod.PUT, requestEntity, Integer.class);
 
         log.debug("REMOTE REQUEST: EXITED FROM THE CHECK_REMOTE_DATA");
         System.out.println("-------------------->"+ response_entity.getBody());
